a8373088a4ca2a2afd4c71eed2bbf8b911251f4b
Eduardo Bedolla <bedo99.cf@gmail.com>
2020-12-03 12:40:44 -0600

    Adjunto de Modal junto con Registro Google y Facebook

---
+++ Autorizacion.js
@@ -0,0 +1,34 @@
+entrarGoogle = () => {
+     
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+
+            var token = result.credential.accessToken;
+            $('#ModalCenter').modal('hide');
+            var user = result.user;
+            console.log(token, user.displayName,user.email,"Google");
+
+        }).catch(function(error) {
+            console.log(error);
+    });
+
+}
+
+entrarFacebook = () => {
+
+    var provider = new firebase.auth.FacebookAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+
+            var token = result.credential.accessToken;
+            
+            $('#ModalCenter').modal('hide');
+            var user = result.user;
+            console.log(token, user.displayName,user.email,"Facebook");
+
+        }).catch(function(error) {
+            console.log(error);
+    });
+
+}
